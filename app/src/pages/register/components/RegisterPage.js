ad371e0bf0df4f3c15a6e2a2cbc4a242b7ebb8b5
Levin Passlick <>
2021-11-12 15:37:11 +0100

    :construction: WIP

--- app/src/pages/register/components/RegisterPage.js
+++ app/src/pages/register/components/RegisterPage.js
@@ -1,23 +1,33 @@
-import React, { Component } from 'react'
-import FirebaseDataProvider from "../../../helpers/Firebasedataprovider"
+import React, {Component} from 'react'
+import FirebaseDataProvider from '../../../helpers/Firebasedataprovider'
+import {Stack, IStackTokens} from '@fluentui/react';
+import {DefaultButton, PrimaryButton} from '@fluentui/react/lib/Button';
+import {TextField, MaskedTextField} from '@fluentui/react/lib/TextField';
+
+const stackTokens: IStackTokens = {childrenGap: 40};
 
 class RegisterPage extends React.Component {
-    componentDidMount = () => {
+  componentDidMount = () => {
 
 
-      //  FirebaseDataProvider.register("testqs@asdsad.de", "test123");
+    //  FirebaseDataProvider.register("testqs@asdsad.de", "test123");
 
-    }
-
-
+  }
 
 
-
-
-
-    render() {
-        return <h1>Deine Mutter</h1>
-    }
+  render() {
+    return <Stack horizontal tokens={stackTokens}>
+      <TextField label="E-Mail " required/>
+      <TextField
+          label="Passwort"
+          type="password"
+          canRevealPassword
+          revealPasswordAriaLabel="Passwort anzeigen"
+      />
+      <DefaultButton text="Standard"/>
+      <PrimaryButton text="Primary"/>
+    </Stack>
+  }
 }
 
 

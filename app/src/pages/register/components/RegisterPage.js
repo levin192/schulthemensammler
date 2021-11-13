08227e7b83aa59364efc4b2816a507c0a4cf2683
vendom2k <borankrk@gmail.com>
2021-11-13 17:23:21 +0100

    login und register page

--- app/src/pages/register/components/RegisterPage.js
+++ app/src/pages/register/components/RegisterPage.js
@@ -1,28 +1,46 @@
-import React from 'react';
-import FirebaseDataProvider from '../../../helpers/Firebasedataprovider';
-import {PrimaryButton} from '@fluentui/react/lib/Button';
-import {TextField} from '@fluentui/react/lib/TextField';
+import React from "react";
+import FirebaseDataProvider from "../../../helpers/Firebasedataprovider";
+import {
+  MessageBar,
+  MessageBarType,
+  PrimaryButton,
+  TextField,
+} from "@fluentui/react";
+import { Navigate } from "react-router";
 
 class RegisterPage extends React.Component {
   constructor(params) {
     super(params);
     this.fb = new FirebaseDataProvider();
     this.state = {
-      email: '',
-      password: '',
+      email: "",
+      password: "",
+      wasSuccessfull: true,
+      errorMessage: "",
+      shouldRedirect: false,
     };
   }
 
-  componentDidMount = () => {
-  };
-
-  registerUser = async () => {
+  registerUser = async (event) => {
+    event.preventDefault();
     const email = this.state.email;
     const password = this.state.password;
 
-    const data = await this.fb.register({email, password});
+    try {
+      const data = await this.fb.register({ email, password });
+      this.setState((state) => {
+        state.shouldRedirect = true;
+        return state;
+      });
+    } catch (error) {
+      console.table(error);
+      this.setState((state) => {
+        state.wasSuccessfull = false;
+        state.errorMessage = error.message;
 
-    console.log(data);
+        return state;
+      });
+    }
   };
 
   handleInputChange = (inputEl) => {
@@ -34,33 +52,44 @@ class RegisterPage extends React.Component {
   };
 
   render() {
+    if (this.state.shouldRedirect) {
+      return <Navigate to="/"></Navigate>;
+    }
+
     return (
-        <>
-          <h1>Schulthemensammler</h1>
-          <form onSubmit={this.registerUser}>
-            <TextField
-                label="E-Mail"
-                id="email"
-                autoComplete="new-email"
-                type="email"
-                required
-                onChange={this.handleInputChange}
-            />
-            <TextField
-                pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}"
-                minlength="6"
-                title="Mindestens eine Zahl, Groß- und Kleinbuchstaben und mindestens 6 Zeichen"
-                required
-                autoComplete="new-password"
-                label="Passwort"
-                type="password"
-                canRevealPassword
-                revealPasswordAriaLabel="Passwort anzeigen"
-                onChange={this.handleInputChange}
-            />
-            <PrimaryButton text="Registrieren" type="submit"/>
-          </form>
-        </>
+      <>
+        <h1>Schulthemensammler</h1>
+
+        <form onSubmit={this.registerUser}>
+          <TextField
+            label="E-Mail"
+            id="email"
+            autoComplete="new-email"
+            type="email"
+            required
+            onChange={this.handleInputChange}
+          />
+          <TextField
+            // pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}"
+            // minlength="6"
+            title="Mindestens eine Zahl, Groß- und Kleinbuchstaben und mindestens 6 Zeichen"
+            required
+            autoComplete="new-password"
+            label="Passwort"
+            type="password"
+            canRevealPassword
+            revealPasswordAriaLabel="Passwort anzeigen"
+            onChange={this.handleInputChange}
+          />
+          <PrimaryButton text="Registrieren" type="submit" />
+        </form>
+
+        {this.state.wasSuccessfull ? null : (
+          <MessageBar messageBarType={MessageBarType.error}>
+            {this.state.errorMessage}
+          </MessageBar>
+        )}
+      </>
     );
   }
 }

567b7c25b3883148f0c98fd90958bd39174828b7
vendom2k <borankrk@gmail.com>
2021-11-13 16:24:20 +0100

    react routen hinzugefügt

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,56 @@
-import RegisterPage from "./pages/register/components/RegisterPage"
-
+import RegisterPage from "./pages/register/components/RegisterPage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-     <RegisterPage></RegisterPage>
+    <Router>
+      <Routes>
+        <Route
+          path="/register"
+          exact
+          element={
+            <>
+              <div>
+                <RegisterPage></RegisterPage>
+              </div>
+            </>
+          }
+        ></Route>
+        <Route
+          path="/login"
+          exact
+          element={
+            <>
+              <div>
+                <h1>Placeholder login</h1>
+              </div>
+            </>
+          }
+        ></Route>
+        <Route
+          path="/"
+          exact
+          element={
+            <>
+              <div>
+                <h1>Placeholder home</h1>
+              </div>
+            </>
+          }
+        ></Route>
+        <Route
+          path="/kalender"
+          exact
+          element={
+            <>
+              <div>
+                <h1>Placeholder kalender</h1>
+              </div>
+            </>
+          }
+        ></Route>
+      </Routes>
+    </Router>
   );
 }
 

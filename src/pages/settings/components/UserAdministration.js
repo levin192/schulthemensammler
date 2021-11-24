c51557490884917980d033aa2e832954952884b3
Levin Passlick <>
2021-11-24 17:01:02 +0100

    BRUh

--- src/pages/settings/components/UserAdministration.js
+++ src/pages/settings/components/UserAdministration.js
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import {DetailsList, DetailsListLayoutMode, SelectionMode} from '@fluentui/react/lib/DetailsList';
 import {TextField} from '@fluentui/react/lib/TextField';
+import {Checkbox} from '@fluentui/react/lib/Checkbox';
+import {ComboBox} from '@fluentui/react/lib/ComboBox';
 import {useState} from 'react';
 
 export const UserAdministration: React.FunctionComponent = (props) => {
@@ -9,27 +11,51 @@ export const UserAdministration: React.FunctionComponent = (props) => {
       return {
         key: index,
         name: name,
-        value: 'dein Vadder'
+        email: 'mail@example.com',
+        admin: false,
+        className: null
       }
     }
     return {
       key: index,
-      name: 'NAME NICHT GESETZT',
-      value: 'dein Vadder'
+      name: null,
+      email: 'mail@example.com',
+      admin: false,
+      className: null
     }
   })
   const [items, setItems] = useState(populateList)
   const [searchText, setSearchText] = useState('')
   console.log(items)
   const columns = [
-    {key: 'column1', name: 'Name', fieldName: 'name', minWidth: 100, maxWidth: 200, isResizable: true},
-    {key: 'column2', name: 'Value', fieldName: 'value', minWidth: 100, maxWidth: 200, isResizable: true},
+    {key: 'nameCol', name: 'Name', fieldName: 'name', minWidth: 100, maxWidth: 200, isResizable: true},
+    {key: 'emailCol', name: 'E-Mail', fieldName: 'value', minWidth: 100, maxWidth: 200, isResizable: true},
+    {key: 'adminCol', name: 'Admin', fieldName: 'admin', minWidth: 50, maxWidth: 200, isResizable: true},
+    {key: 'classCol', name: 'Klasse', fieldName: 'classSelect', minWidth: 150, maxWidth: 200, isResizable: true},
   ]
   const originalItems = items
   const onFilterChanged = (element) => {
     setSearchText(element.target.value)
-    setItems(searchText ? originalItems.filter(i => i.name.toLowerCase().indexOf(searchText) > -1) : items,)
+    //setItems(searchText ? originalItems.filter(i => i.name.toLowerCase().indexOf(searchText) > -1) : items,)
   };
+  const renderItemColumn = (item, index, column) => {
+    console.log(item)
+    console.log(index)
+    switch (column.key) {
+      case 'nameCol':
+        return (!item.name)?(<span style={{color: 'red'}}>NAME NICHT GESETZT</span>):(<span>{item.name}</span>)
+      case 'emailCol':
+        return (<a href={'mailto:' + item.email}>{item.email}</a>)
+      case 'adminCol':
+        return (<Checkbox/>)
+      case 'classCol':
+        return (<ComboBox options={[  { key: 'A', text: 'Option A' },
+          { key: 'B', text: 'Option B' },
+          { key: 'C', text: 'Option C' },
+          { key: 'D', text: 'Option D' },
+        ]}/>)
+    }
+  }
   return (
       <>
         <TextField
@@ -42,6 +68,7 @@ export const UserAdministration: React.FunctionComponent = (props) => {
             compact={false}
             columns={columns}
             selectionMode={SelectionMode.none}
+            onRenderItemColumn={renderItemColumn}
             // getKey={this._getKey}
             // setKey="none"
             layoutMode={DetailsListLayoutMode.justified}

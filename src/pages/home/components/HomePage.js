a18c90de9561b3e6f473210d01e7a8d4f67237a5
Levin Passlick <>
2021-11-25 19:15:40 +0100

    💏👩‍❤️‍💋‍👩

--- src/pages/home/components/HomePage.js
+++ src/pages/home/components/HomePage.js
@@ -1,21 +1,49 @@
-import { DefaultButton, PrimaryButton } from "@fluentui/react";
-import React from "react";
-import { Link } from "react-router-dom";
+import {CompoundButton, PrimaryButton} from '@fluentui/react/lib/Button';
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {ReactComponent as SideSVG} from '../../../svg/space.svg';
+import {Store} from '../../../helpers/Store'
 
 class HomePage extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Placeholder home</h1>
-        <Link to="/login">
-          <PrimaryButton text="Login" />
-        </Link>
-        <Link to="/register">
-          <DefaultButton text="Register" />
-        </Link>
-      </div>
+        <>
+          <div>
+            <h1>Sammelt eure Schultheme gemeinsam - egal wo ihr seid!</h1>
+            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
+              et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+              Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
+              amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
+              aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
+              gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+            {(!this.context.isRegisteredUser) ? (
+                <>
+                  <Link to="/register">
+                    <CompoundButton primary secondaryText="Für neue Nutzer" style={{marginRight: '15px'}}>
+                      Registrieren
+                    </CompoundButton>
+                  </Link>
+                  <Link to="/login">
+                    <CompoundButton secondaryText="Für registrierte Nutzer">
+                      Anmelden
+                    </CompoundButton>
+                  </Link>
+                </>
+            ) : (
+                <>
+                  <Link to="/calendar">
+                    <PrimaryButton text="Zum Kalender" />
+                  </Link>
+                </>
+            )}
+          </div>
+          <div>
+            <SideSVG/>
+          </div>
+        </>
     );
   }
+  static contextType = Store;
 }
 
 export default HomePage;

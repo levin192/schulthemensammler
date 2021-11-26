58c0ca37e8c507e09a54d4647d9815cf0461a346
hoppelino <77492734+hoppelino@users.noreply.github.com>
2021-11-26 10:56:19 +0100

    hoppe und körük und hatam bisschen

--- src/pages/settings/components/SchoolDayPickerComponent.js
+++ src/pages/settings/components/SchoolDayPickerComponent.js
@@ -1,72 +1,78 @@
-import React from 'react';
-import {Dropdown, DropdownMenuItemType, IDropdownOption} from '@fluentui/react/lib/Dropdown';
+import React from "react";
+import { Dropdown, DropdownMenuItemType } from "@fluentui/react/lib/Dropdown";
 import FirebaseDataProvider from "../../../helpers/Firebasedataprovider";
-
-
-const options: IDropdownOption[] = [
-    {key: 'dayHeader', text: 'Tage', itemType: DropdownMenuItemType.Header},
-    {key: 'keyMonday', text: 'Montag'},
-    {key: '2', text: 'Dienstag'},
-    {key: '3', text: 'Mittwoch'},
-    {key: '4', text: 'Donnerstag'},
-    {key: '6', text: 'Freitag'},
-    {key: '-', text: '-', itemType: DropdownMenuItemType.Divider},
-    {key: '7', text: 'Samstag'},
-    {key: '0', text: 'Sonntag'},
-];
+import { TextField } from "@fluentui/react";
 
 export default class SchoolDayPicker extends React.Component {
-    constructor() {
-        super();
-        this.fb = new FirebaseDataProvider();
-        this.state = {
-            monday: false, tuesday: false, wednesday: false, thursday: false, friday: false, saturday: false, sunday: false
-        };
-    }
+  constructor() {
+    super();
+    this.fb = new FirebaseDataProvider();
+    this.state = {
+      monday: false,
+      tuesday: false,
+      wednesday: false,
+      thursday: false,
+      friday: false,
+      saturday: false,
+      sunday: false
+    };
 
-    componentDidMount = () => {
-        this.getSchooldays()
-    }
+    this.options = [
+      { key: "dayHeader", text: "Tage", itemType: DropdownMenuItemType.Header },
+      { key: "keyMonday", text: "Montag" },
+      { key: "2", text: "Dienstag" },
+      { key: "3", text: "Mittwoch" },
+      { key: "4", text: "Donnerstag" },
+      { key: "6", text: "Freitag" },
+      { key: "-", text: "-", itemType: DropdownMenuItemType.Divider },
+      { key: "7", text: "Samstag" },
+      { key: "0", text: "Sonntag" }
+    ];
+  }
 
-    // handleChange = (inputEl) => {
+  componentDidMount = () => {
+    this.getSchooldays();
+  };
+
+  getSchooldays = (className) => {
+    // const userId = this.fb.firebase.auth().currentUser.uid
+    // this.fb.firebase
+    //   .firestore()
+    //   .collection("klassen")
+    //   .doc("6A") // KlassenID
+    //   .onSnapshot((querySnapshot) => {
+    //     const schooldays = querySnapshot.data();
     //     this.setState((state) => {
-    //         state[inputEl.target.id] = inputEl.target.value;
-    //         console.log(state)
-    //         console.log(inputEl.currentTarget.id)
-    //         console.log(inputEl.currentTarget.value)
-    //         return state;
+    //       state.monday = schooldays.montag;
+    //       state.tuesday = schooldays.dienstag;
+    //       state.wednesday = schooldays.mittwoch;
+    //       state.thursday = schooldays.donnerstag;
+    //       state.friday = schooldays.freitag;
+    //       state.saturday = schooldays.samstag;
+    //       state.sunday = schooldays.sonntag;
+    //       return state;
     //     });
-    // }
+    //   });
+  };
 
-    getSchooldays = () => {
-       // const userId = this.fb.firebase.auth().currentUser.uid
-
-        this.fb.firebase.firestore().collection('settings').doc('schultage').onSnapshot((querySnapshot) => {
-            const schooldays = querySnapshot.data()
-            this.setState(state => {
-                state.monday = schooldays.montag
-                state.tuesday = schooldays.dienstag
-                state.wednesday = schooldays.mittwoch
-                state.thursday = schooldays.donnerstag
-                state.friday = schooldays.freitag
-                state.saturday = schooldays.samstag
-                state.sunday = schooldays.sonntag
-                return state;
-            })
-        })
-    }
-
-    render () {
-        return (
-            <Dropdown
-                placeholder="Select options"
-                label="Schultage auswählen"
-                multiSelect
-                options={options}
-                // styles={dropdownStyles}
-                onChange={this.handleChange}
-            />
-        )
-    }
-};
-
+  render() {
+    return (
+      <>
+        <TextField
+          id="class"
+          label={"Klasse"}
+          placeholder={"Klasse"}
+          required
+        />
+        <Dropdown
+          placeholder="Select options"
+          label="Schultage auswählen"
+          multiSelect
+          options={this.options}
+          // styles={dropdownStyles}
+          onChange={this.handleChange}
+        />
+      </>
+    );
+  }
+}

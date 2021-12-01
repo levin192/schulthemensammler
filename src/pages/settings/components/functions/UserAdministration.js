b49e0acedbf17ebddca3cf619e847009e94a5656
Levin Passlick <>
2021-12-01 15:00:04 +0100

    use state for change list

--- src/pages/settings/components/functions/UserAdministration.js
+++ src/pages/settings/components/functions/UserAdministration.js
@@ -1,9 +1,5 @@
 import * as React from "react";
-import {
-  DetailsList,
-  DetailsListLayoutMode,
-  SelectionMode
-} from "@fluentui/react/lib/DetailsList";
+import { DetailsList, DetailsListLayoutMode, SelectionMode } from "@fluentui/react/lib/DetailsList";
 import { TextField } from "@fluentui/react/lib/TextField";
 import { Checkbox } from "@fluentui/react/lib/Checkbox";
 import { ComboBox } from "@fluentui/react/lib/ComboBox";
@@ -14,22 +10,21 @@ import { useState } from "react";
 export const UserAdministration = (props) => {
   const fb = props.fireBase;
   const userList = () =>
-    props.userList
-      .filter((userObj) => userObj.username) // filter out users who have not set username
-      .filter((userObj) => userObj.username !== props.currentUserName) // filter out self
-      .map((userObj, index) => {
-        // For readability and add key
-        return {
-          key: index,
-          userName: userObj.username,
-          firstName: userObj.firstname,
-          lastName: userObj.lastname,
-          email: userObj.email,
-          admin: userObj.isAdmin,
-          schoolClasses: userObj.schoolClasses
-        };
-      });
-
+      props.userList
+          .filter((userObj) => userObj.username) // filter out users who have not set username
+          .filter((userObj) => userObj.username !== props.currentUserName) // filter out self
+          .map((userObj, index) => {
+            // For readability and add key
+            return {
+              key: index,
+              userName: userObj.username,
+              firstName: userObj.firstname,
+              lastName: userObj.lastname,
+              email: userObj.email,
+              admin: userObj.isAdmin,
+              schoolClasses: userObj.schoolClasses,
+            };
+          });
   const allSchoolClasses = props.schoolClassList
       .filter((schoolClass) => schoolClass.name.length > 0) // filter empty
       .map((schoolClass) => {
@@ -38,13 +33,13 @@ export const UserAdministration = (props) => {
           text: schoolClass.name,
         };
       });
-
   const [userItems, setUserItems] = useState(userList);
   const [originalItems] = useState(userItems);
   const [filteredItems] = useState(userItems);
   const [isSaving, setIsSaving] = useState(false);
-  // const [hasChanges, setHasChanges] = useState(false);
+  const [hasChanges, setHasChanges] = useState(false);
   const [currentUserComboBox, setCurrentUserComboBox] = useState(undefined);
+  const [changesList] = useState([]);
   const columns = [
     {
       key: "userNameCol",
@@ -52,7 +47,7 @@ export const UserAdministration = (props) => {
       fieldName: "userName",
       minWidth: 100,
       maxWidth: 200,
-      isResizable: true
+      isResizable: true,
     },
     {
       key: "fullNameCol",
@@ -60,7 +55,7 @@ export const UserAdministration = (props) => {
       fieldName: "fullName",
       minWidth: 100,
       maxWidth: 300,
-      isResizable: true
+      isResizable: true,
     },
     {
       key: "emailCol",
@@ -68,7 +63,7 @@ export const UserAdministration = (props) => {
       fieldName: "value",
       minWidth: 100,
       maxWidth: 200,
-      isResizable: true
+      isResizable: true,
     },
     {
       key: "adminCol",
@@ -76,7 +71,7 @@ export const UserAdministration = (props) => {
       fieldName: "admin",
       minWidth: 50,
       maxWidth: 50,
-      isResizable: true
+      isResizable: true,
     },
     {
       key: "schoolClassCol",
@@ -84,25 +79,24 @@ export const UserAdministration = (props) => {
       fieldName: "SchoolClassSelect",
       minWidth: 150,
       maxWidth: 150,
-      isResizable: true
-    }
+      isResizable: true,
+    },
   ];
-  const changesList = [];
   const onFilterChanged = (element) => {
     const searchText = element.target.value.toLowerCase();
     setUserItems(
-      searchText
-        ? filteredItems.filter(
+        searchText
+            ? filteredItems.filter(
             (i) =>
-              i.userName.toLowerCase().indexOf(searchText) > -1 ||
-              i.firstName.toLowerCase().indexOf(searchText) > -1 ||
-              i.lastName.toLowerCase().indexOf(searchText) > -1
-          )
-        : originalItems
+                i.userName.toLowerCase().indexOf(searchText) > -1 ||
+                i.firstName.toLowerCase().indexOf(searchText) > -1 ||
+                i.lastName.toLowerCase().indexOf(searchText) > -1,
+            )
+            : originalItems,
     );
   };
   const emptyEntry = () => {
-    return <span style={{ color: "lightgray" }}>nicht gesetzt</span>;
+    return <span style={{color: "lightgray"}}>nicht gesetzt</span>;
   };
   const renderItemColumn = (user, index, column) => {
     switch (column.key) {
@@ -114,42 +108,43 @@ export const UserAdministration = (props) => {
             {user.firstName}&nbsp;{user.lastName}
           </span>
         ) : (
-          emptyEntry()
+            emptyEntry()
         );
       case "emailCol":
         return user.email ? (
-          <a href={"mailto:" + user.email}>{user.email}</a>
+            <a href={"mailto:" + user.email}>{user.email}</a>
         ) : (
-          emptyEntry()
+            emptyEntry()
         );
       case "adminCol":
         return user.admin ? (
-          <Checkbox
-            id={user.userName}
-            defaultChecked
-            onChange={onAdminChange}
-          />
+            <Checkbox
+                id={user.userName}
+                defaultChecked
+                onChange={onAdminChange}
+            />
         ) : (
-          <Checkbox id={user.userName} onChange={onAdminChange} />
+            <Checkbox id={user.userName} onChange={onAdminChange}/>
         );
       case "schoolClassCol":
         return (
-          <div className="combo-wrapper" data-user-ref={user.userName}>
-            <ComboBox
-              multiSelect
-              //autoComplete="on"
-              options={allSchoolClasses}
-              onChange={() => onSchoolClassesChange(user.userName)}
-              defaultSelectedKey={user.schoolClasses}
-              onMenuDismiss={onSchoolClassesChangeFinished}
-            />
-          </div>
+            <div className="combo-wrapper" data-user-ref={user.userName}>
+              <ComboBox
+                  multiSelect
+                  autoComplete="on"
+                  options={allSchoolClasses}
+                  onChange={() => onSchoolClassesChange(user.userName)}
+                  defaultSelectedKey={user.schoolClasses}
+                  onMenuDismiss={onSchoolClassesChangeFinished}
+              />
+            </div>
         );
       default:
         return null;
     }
   };
   const onAdminChange = (e) => {
+    setHasChanges(true);
     const userName = e.target.id;
     const isAdmin = e.target.checked;
     const unsavedChange = changesList.find((x) => x.userName === userName); // If is in Array already, so we only need to update the isAdmin prop in the object
@@ -158,7 +153,7 @@ export const UserAdministration = (props) => {
     } else {
       changesList.push({
         userName: userName,
-        isAdmin
+        isAdmin,
       });
     }
   };
@@ -166,21 +161,22 @@ export const UserAdministration = (props) => {
     setCurrentUserComboBox(userRef);
   };
   const onSchoolClassesChangeFinished = () => {
-    if (window.document.querySelector('[data-user-ref="' + currentUserComboBox + '"]')) {
+    setHasChanges(true);
+    if (window.document.querySelector("[data-user-ref=\"" + currentUserComboBox + "\"]")) {
       const schoolClasses = window.document
-        .querySelector('[data-user-ref="' + currentUserComboBox + '"]')
-        .querySelector("input")
-        .value.replace(/\s+/g, "")
-        .split(",");
+          .querySelector("[data-user-ref=\"" + currentUserComboBox + "\"]")
+          .querySelector("input")
+          .value.replace(/\s+/g, "")
+          .split(",");
       const unsavedChange = changesList.find(
-        (x) => x.userName === currentUserComboBox
+          (x) => x.userName === currentUserComboBox,
       ); // If is in Array already
       if (unsavedChange) {
         unsavedChange.schoolClasses = schoolClasses;
       } else {
         changesList.push({
           userName: currentUserComboBox,
-          schoolClasses: schoolClasses
+          schoolClasses: schoolClasses,
         });
       }
     }
@@ -192,7 +188,6 @@ export const UserAdministration = (props) => {
     }, 1500);
   };
   const onSave = async (e) => {
-    console.log(changesList);
     e.preventDefault();
     if (changesList.length > 0) {
       setIsSaving(true);
@@ -200,67 +195,67 @@ export const UserAdministration = (props) => {
         const isAdmin = item.isAdmin;
         const schoolClasses = item.schoolClasses;
         fb.firebase
-          .firestore()
-          .collection("Users")
-          .where("username", "==", item.userName)
-          .get()
-          .then((r) => {
-            const x = fb.firebase
-              .firestore()
-              .collection("Users")
-              .doc(r.docs[0].id);
-            if (
-              item.hasOwnProperty("isAdmin") &&
-              item.hasOwnProperty("schoolClasses")
-            ) {
-              x.update({
-                isAdmin,
-                schoolClasses: schoolClasses
-              }).then(hideSavingSpinner);
-            } else {
-              if (item.hasOwnProperty("isAdmin")) {
+            .firestore()
+            .collection("Users")
+            .where("username", "==", item.userName)
+            .get()
+            .then((r) => {
+              const x = fb.firebase
+                  .firestore()
+                  .collection("Users")
+                  .doc(r.docs[0].id);
+              if (
+                  item.hasOwnProperty("isAdmin") &&
+                  item.hasOwnProperty("schoolClasses")
+              ) {
                 x.update({
-                  isAdmin
+                  isAdmin,
+                  schoolClasses: schoolClasses,
                 }).then(hideSavingSpinner);
+              } else {
+                if (item.hasOwnProperty("isAdmin")) {
+                  x.update({
+                    isAdmin,
+                  }).then(hideSavingSpinner);
+                }
+                if (item.hasOwnProperty("schoolClasses")) {
+                  x.update({
+                    schoolClasses: schoolClasses,
+                  }).then(hideSavingSpinner);
+                }
               }
-              if (item.hasOwnProperty("schoolClasses")) {
-                x.update({
-                  schoolClasses: schoolClasses
-                }).then(hideSavingSpinner);
-              }
-            }
-          });
+            });
       });
     }
   };
   return (
-    <>
-      <TextField label={"Liste filtern:"} onChange={onFilterChanged} />
-      <div className="user-admin-list-wrap">
-        <div className="user-admin-list-content">
-          <DetailsList
-            items={userItems}
-            compact={false}
-            columns={columns}
-            selectionMode={SelectionMode.none}
-            onRenderItemColumn={renderItemColumn}
-            layoutMode={DetailsListLayoutMode.justified}
-            isHeaderVisible={true}
-          />
+      <>
+        <TextField label={"Liste filtern:"} onChange={onFilterChanged}/>
+        <div className="user-admin-list-wrap">
+          <div className="user-admin-list-content">
+            <DetailsList
+                items={userItems}
+                compact={false}
+                columns={columns}
+                selectionMode={SelectionMode.none}
+                onRenderItemColumn={renderItemColumn}
+                layoutMode={DetailsListLayoutMode.justified}
+                isHeaderVisible={true}
+            />
+          </div>
+          <div
+              className={
+                isSaving
+                    ? "user-admin-list-spinner visible"
+                    : "user-admin-list-spinner"
+              }
+          >
+            <Spinner label="Speichern..."/>
+          </div>
         </div>
-        <div
-          className={
-            isSaving
-              ? "user-admin-list-spinner visible"
-              : "user-admin-list-spinner"
-          }
-        >
-          <Spinner label="Speichern..." />
-        </div>
-      </div>
-      <PrimaryButton onClick={onSave} disabled={isSaving}>
-        Änderungen speichern
-      </PrimaryButton>
-    </>
+        <PrimaryButton onClick={onSave} disabled={!(hasChanges)}>
+          Änderungen speichern
+        </PrimaryButton>
+      </>
   );
 };

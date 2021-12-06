daf80585995eaf9c249f90a63cd61ed62490ab35
Levin Passlick <>
2021-12-06 18:43:09 +0100

    calendarstringsintomydatahole

--- src/pages/calendar/components/CalendarComponent.js
+++ src/pages/calendar/components/CalendarComponent.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {calendarStrings} from './data/calendarStrings';
 import { Calendar, DayOfWeek } from "@fluentui/react";
 
 const GetTextField = (props) => {
@@ -11,55 +12,6 @@ const GetTextField = (props) => {
     props.onCalenderClick(dateObj, dateId);
   };
 
-  const calendarStrings = {
-    months: [
-      "Januar",
-      "Februar",
-      "März",
-      "April",
-      "Mai",
-      "Juni",
-      "Juli",
-      "August",
-      "September",
-      "Oktober",
-      "November",
-      "Dezember",
-    ],
-
-    shortMonths: [
-      "Jan",
-      "Feb",
-      "Mär",
-      "Apr",
-      "Mai",
-      "Jun",
-      "Jul",
-      "Aug",
-      "Sep",
-      "Okt",
-      "Nov",
-      "Dez",
-    ],
-
-    days: [
-      "Sonntag",
-      "Montag",
-      "Dienstag",
-      "Mittwoch",
-      "Donnerstag",
-      "Freitag",
-      "Samstag",
-    ],
-
-    shortDays: ["SO", "MO", "DI", "MI", "DO", "FR", "SA"],
-
-    goToToday: "Gehe zum aktuellen Monat",
-    prevMonthAriaLabel: "Gehe zum vorherigen Monat",
-    nextMonthAriaLabel: "Gehe zum nächsten Monat",
-    prevYearAriaLabel: "Gehe zum vorherigen Jahr",
-    nextYearAriaLabel: "Gehe zum nächsten Jahr",
-  };
 
   return (
     <>
---
+++ src/pages/calendar/components/data/calendarStrings.js
@@ -0,0 +1,49 @@
+export const calendarStrings = {
+  months: [
+    "Januar",
+    "Februar",
+    "März",
+    "April",
+    "Mai",
+    "Juni",
+    "Juli",
+    "August",
+    "September",
+    "Oktober",
+    "November",
+    "Dezember",
+  ],
+
+  shortMonths: [
+    "Jan",
+    "Feb",
+    "Mär",
+    "Apr",
+    "Mai",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Okt",
+    "Nov",
+    "Dez",
+  ],
+
+  days: [
+    "Sonntag",
+    "Montag",
+    "Dienstag",
+    "Mittwoch",
+    "Donnerstag",
+    "Freitag",
+    "Samstag",
+  ],
+
+  shortDays: ["SO", "MO", "DI", "MI", "DO", "FR", "SA"],
+
+  goToToday: "Gehe zum aktuellen Monat",
+  prevMonthAriaLabel: "Gehe zum vorherigen Monat",
+  nextMonthAriaLabel: "Gehe zum nächsten Monat",
+  prevYearAriaLabel: "Gehe zum vorherigen Jahr",
+  nextYearAriaLabel: "Gehe zum nächsten Jahr",
+};
